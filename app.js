a7715bd3cfa36a799a60c36edad12e49d12b4165
chirthKKK <chirathpathinayaka@gmail.com>
2024-11-04 17:57:56 +0530

    update with country detais

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 function search() {
 
-
-
   fetch("https://restcountries.com/v3.1/all")
     .then(res => res.json())
     .then(data => {
@@ -36,14 +34,18 @@ let searchvalue=document.getElementById("txt_country").value;
 let need=document.getElementById("countryneeded");
 
 console.log(searchvalue);
-  fetch(`https://restcountries.com/v3.1/name/${searchvalue}`)
+  fetch(`https://restcountries.com/v3.1/name/${searchvalue}?fullText=true`)
     .then(res => res.json())
     .then(data => {data.forEach(obj => {
 
       need.innerHTML=obj.name.common;
       document.getElementById("img").innerHTML=`<img src="${obj.flags.png}" alt="">`
 
-      document.getElementById("map").innerHTML=`<a  href="${obj.maps.googleMaps}">Go to Map</a> `;
+      document.getElementById("map").innerHTML=`<a  href="${obj.maps.googleMaps}">Go to Google Map</a> `;
+      document.getElementById("population").innerHTML=`population of the country : ${obj.population}`;
+      document.getElementById("name_official").innerHTML=`Official Name : ${obj.name.official}`;
+      document.getElementById("capital").innerHTML=`Capital : ${obj.capital}`;
+      document.getElementById("area").innerHTML=`Area : ${obj.area}`; 
       
     });
       
